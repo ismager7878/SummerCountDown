306b0f9a2fd443b6cdd3307f956c1cfa21d4ab5b
ismager7878 <ao.barbre@gmail.com>
2021-04-13 11:39:05 +0200

    fe

--- Scrpits/memeManager.js
+++ Scrpits/memeManager.js
@@ -1,5 +1,5 @@
 
-var targetDate = new Date("06/25/2021"); 
+var targetDate = new Date("06/23/2021"); 
 
 
 var x = self.setInterval(function(){
@@ -10,13 +10,16 @@ var days = Math.floor(timeBetween / (1000*3600*24));
 var hours = Math.floor(timeBetween / (1000*3600)-(days*24))
 var minuttes = Math.floor(timeBetween / (1000*60)-(days*24*60 + hours*60))
 var seconds = Math.floor(timeBetween / 1000 - (days*24*3600 + hours*3600 + minuttes*60))
-console.log(days);
-console.log(hours);
-console.log(minuttes);
-console.log(seconds);
-document.getElementById("days").innerHTML = days;
-document.getElementById("hours").innerHTML = hours;
-document.getElementById("minuttes").innerHTML = minuttes;
-document.getElementById("seconds").innerHTML = seconds;
 
+var fDays = ("0" + days).slice(-2); 
+var fHours = ("0" + hours).slice(-2); 
+var fMinuttes = ("0" + minuttes).slice(-2); 
+var fSeconds = ("0" + seconds).slice(-2); 
+
+console.log(fDays);
+console.log(fHours);
+console.log(fMinuttes);
+console.log(fSeconds);
+
+document.getElementById("days").innerHTML = fDays + " : " + fHours + " : " + fMinuttes + " : " + fSeconds;
 },1000)
